05744563d73c84db7111969d574a2f51dd9dc800
rhettcaptain <425237277@qq.com>
2017-05-20 15:57:55 +0800

    dev

--- pkg_pose/src/pose.cpp
+++ pkg_pose/src/pose.cpp
@@ -1,34 +1,35 @@
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include <geometry_msgs/PointStamped.h>		
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>			
 
 ros::Publisher reviseOdometryPublisher;
 ros::Publisher robotPosePublisher;
 
+float x;
+float y;
+float th;
+float vx;
+float vy;
+float vth;
 
 //odom-map转换监听，并发布odom的地图坐标
 void transOdom(const tf::TransformListener& listener)
 {
-	geometry_msgs::PointStamped odom_point;
-	laser_point.header.frame_id = "base_laser";
-	laser_point.header.stamp = ros::Time();
-	laser_point.point.x = 1.0;
-	laser_point.point.y = 0.2;
-	laser_point.point.z = 0.0;
+	
 }
 
 //odometry数据更新
 void handleOdom(const nav_msgs::Odometry::ConstPtr& msg)
 {
-	geometry_msgs::PoseWithCovarianceStamped poseMsg;
-	float x = msg->pose.pose.position.x;
-	float y = msg->pose.pose.position.y;
-	float th = tf::getYaw(msg->pose.pose.orientation);
-	float vx = msg->twist.twist.linear.x;
-	float vy = msg->twist.twist.linear.y;
-	float vth = msg->twist.twist.angular.z;
+	x = msg->pose.pose.position.x;
+	y = msg->pose.pose.position.y;
+	th = tf::getYaw(msg->pose.pose.orientation);
+	vx = msg->twist.twist.linear.x;
+	vy = msg->twist.twist.linear.y;
+	vth = msg->twist.twist.angular.z;
 }
 
 //amcl数据直接发布
